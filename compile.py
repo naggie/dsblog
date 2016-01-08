8ac6771fbd557f98f781591aaca78b43ba4a568f
Callan Bryant <callan.bryant@gmail.com>
2016-01-08 06:30:28 +0000

    run in place

--- compile.py
+++ compile.py
@@ -1,18 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
 import os
-from shutil import copytree
+from shutil import copytree,rmtree
 
 build_dir = 'build/'
 
-if os.path.exists(build_dir):
-    raise IOError('Remove build directory first')
+#if os.path.exists(build_dir):
+#    raise IOError('Remove build directory first')
 
 
-#
+build_static_dir = os.path.join(build_dir,'static')
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-
-copytree('static', os.path.join(build_dir,'static') )
+if os.path.exists(build_static_dir):
+    rmtree(build_static_dir)
+copytree('static',build_static_dir)
 
 
 env = Environment(loader=FileSystemLoader('templates'))
