7ac50d866ed49d7f071389a2f27eeeacf716591c
Callan Bryant <callan.bryant@gmail.com>
2016-01-08 05:43:23 +0000

    static dir as part of build process

--- compile.py
+++ compile.py
@@ -1,11 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
 import os
+from shutil import copytree
 
-output_directory = 'www/'
+build_dir = 'build/'
+#
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+copytree('static', os.path.join(build_dir,'static') )
 
 
 env = Environment(loader=FileSystemLoader('templates'))
 
 template = env.get_template('blog.html')
-filepath = os.path.join(output_directory,'index.html')
+filepath = os.path.join(build_dir,'index.html')
 template.stream().dump(filepath)
