aff6da9b6e10a96fe1895988f5ceedb0fd212306
Callan Bryant <callan.bryant@gmail.com>
2016-01-10 17:57:58 +0000

    crawl comments

--- crawlers/discourse.py
+++ crawlers/discourse.py
@@ -30,6 +30,41 @@ class Discourse():
 
         return response.json()
 
+    def normalise_html(self,html):
+        content = BeautifulSoup(html,'html.parser')
+        # repair all protocol-less URLS
+        for img in content.find_all('img'):
+            if img['src'].startswith('//'):
+                img['src'] = self.url.split('//')[0] + img['src']
+
+        for a in content.find_all('a'):
+            if a['href'].startswith('//'):
+                a['href'] = self.url.split('//')[0] + a['href']
+
+        # remove useless meta links (a discourse-ism)
+        for div in content.find_all('div',class_="meta"):
+            div.extract()
+
+        return unicode(content)
+
+
+    def find_image(self,html):
+        'look for best image URL or None, for header image'
+        content = BeautifulSoup(html,'html.parser')
+        image = None
+        for img in content.find_all('img'):
+            try:
+                if 'emoji' in img['class'] or 'avatar' in img['class']:
+                    continue
+            except KeyError:
+                pass
+
+            image = img['src']
+            break
+
+        return image
+
+
     def list_articles(self,name='Blog'):
         # find cetegory ID
         for cat in self.get(['categories'])['category_list']['categories']:
@@ -44,7 +79,7 @@ class Discourse():
         for t in self.get(['c',id])['topic_list']['topics']:
             ids.append(t["id"])
 
-        # don't want this
+        # don't want any category definition posts
         category_definition_id = min(ids)
         ids.remove(category_definition_id)
 
@@ -55,42 +90,28 @@ class Discourse():
         for id in tqdm(ids,leave=True):
             topic = self.get(['t',id])
             first_post = topic['post_stream']['posts'][0]
+            content = self.normalise_html(first_post['cooked'])
 
-            content = BeautifulSoup(first_post['cooked'],'html.parser')
-            # repair all protocol-less URLS
-            for img in content.find_all('img'):
-                if img['src'].startswith('//'):
-                    img['src'] = self.url.split('//')[0] + img['src']
+            comments = list()
+            for post in topic['post_stream']['posts'][1:]:
+                comments.append({
+                    "content": self.normalise_html(post['cooked']),
+                    "author_name": post['display_username'],
+                    "author_image": self.url+post["avatar_template"].format(size=200),
+                })
 
-            for a in content.find_all('a'):
-                if a['href'].startswith('//'):
-                    a['href'] = self.url.split('//')[0] + a['href']
-
-            # remove useless meta links (a discourse-ism)
-            for div in content.find_all('div',class_="meta"):
-                div.extract()
-
-            # look for best image
-            image = None
-            for img in content.find_all('img'):
-                try:
-                    if 'emoji' in img['class'] or 'avatar' in img['class']:
-                        continue
-                except KeyError:
-                    pass
-
-                image = img['src']
-                break
 
             articles.append({
                 "title":topic['title'],
                 "url": '/'.join([self.url,'t',topic['slug'],str(id)]),
-                "image": image, # first image
+                "comments_url": '/'.join([self.url,'t',topic['slug'],str(id)]),
+                "image": self.find_image(content),
                 # fix internal image URLS which don't have protocol
-                "content":unicode(content),
+                "content":content,
                 "author_image":self.url+first_post["avatar_template"].format(size=200),
                 "author_name":first_post["display_username"],
                 "published":parse_date(first_post['created_at']),
+                "comments":comments,
             })
 
         return articles
