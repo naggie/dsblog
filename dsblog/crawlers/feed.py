a01ed706afda861f2484d8aeedd553ca135a2a47
Callan Bryant <callan.bryant@gmail.com>
2016-01-25 22:07:56 +0000

    support auto full articles from rss

--- dsblog/crawlers/feed.py
+++ dsblog/crawlers/feed.py
@@ -5,21 +5,27 @@ from datetime import datetime
 import pytz
 
 class Feed(Crawler):
-    def __init__(self,url,username,full_articles=False):
+    def __init__(self,url,username):
         super(Feed,self).__init__(url)
         self.username = username
-        self.full_articles = full_articles
 
     def crawl(self):
         d = feedparser.parse(self.url)
 
         for entry in d["entries"]:
+            if 'content' in entry:
+                content = entry["content"][0]["value"]
+                full_article = True
+            else:
+                content = entry["summary"]
+                full_article = False
+
             self.articles.append({
                 "title":entry['title'],
                 "url": entry["link"],
-                "content":entry["summary"] if self.full_articles else None,
+                "content":content if full_article else None,
                 # still run through excerpt generator to remove images if they exist
-                "excerpt": self.generate_excerpt(entry["summary"]),
+                "excerpt": self.generate_excerpt(content),
                 "username":self.username,
                 "comments_url": entry['link'],
                 "image": self.find_image(entry["summary"]),
