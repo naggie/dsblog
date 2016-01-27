d0494455b6251cffde596280f8ada36fb2e2f3e7
Callan Bryant <callan.bryant@gmail.com>
2016-01-27 18:28:59 +0000

    explicit full articles, again

--- dsblog/crawlers/feed.py
+++ dsblog/crawlers/feed.py
@@ -5,9 +5,10 @@ from datetime import datetime
 import pytz
 
 class Feed(Crawler):
-    def __init__(self,url,username):
+    def __init__(self,url,username,full_articles=True):
         super(Feed,self).__init__(url)
         self.username = username
+        self.full_articles = full_articles
 
     def crawl(self):
         d = feedparser.parse(self.url)
@@ -15,15 +16,13 @@ class Feed(Crawler):
         for entry in d["entries"]:
             if 'content' in entry:
                 content = entry["content"][0]["value"]
-                full_article = True
             else:
                 content = entry["summary"]
-                full_article = False
 
             self.articles.append({
                 "title":entry['title'],
                 "url": entry["link"],
-                "content":content if full_article else None,
+                "content":content if self.full_articles else None,
                 # still run through excerpt generator to remove images if they exist
                 "excerpt": self.generate_excerpt(content),
                 "username":self.username,
