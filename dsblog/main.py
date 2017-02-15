0ccbdb5ac8a40b0b55175faf8b08a39a9dc3c693
Callan Bryant <callan.bryant@gmail.com>
2017-02-15 22:17:36 +0000

    optional nav links

--- dsblog/main.py
+++ dsblog/main.py
@@ -85,6 +85,18 @@ def main():
     env.globals["logo_size"] = config["logo_size"]
     env.globals["background"] = config["background"]
 
+    nav_links = [
+        ('Blog','.'),
+    ]
+    if config.get("enable_about_page",True):
+        nav_links.append(('About','about/'))
+
+    nav_links.append(config["extra_links"])
+
+    # one is useless
+    if len(nav_links) == 1:
+        nav_links = []
+
 
     template = env.get_template('blog.html')
     filepath = join(config['output_dir'],'index.html')
